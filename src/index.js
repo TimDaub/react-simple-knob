5bfaee4d94719ead0ff7915ef1468d78d320576e
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-12-11 19:44:39 +0100

    Add .babelrc

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
+// @format
 import React from "react";
 
 export default class Knob extends React.Component {
   render() {
-    return "hello";
+    return (
+      <svg viewBox="0 0 100, 100">
+        <circle r="40" cx="50" cy="50" />
+      </svg>
+    );
   }
 }
