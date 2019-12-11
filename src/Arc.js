2b15d36f918bbd20b887c91a342600cadf546d96
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-12-11 20:52:40 +0100

    First working version of knob

---
+++ src/Arc.js
@@ -0,0 +1,50 @@
+// @format
+// Taken from: https://github.com/eskimoblood/rc-knob
+import React from "react";
+
+const pointOnCircle = (center, radius, angle) => ({
+  x: center + radius * Math.cos(angle),
+  y: center + radius * Math.sin(angle)
+});
+const degTorad = deg => (Math.PI * deg) / 180;
+
+const calcPath = ({
+  percentage,
+  angleOffset,
+  angleRange,
+  arcWidth,
+  radius: outerRadius,
+  center
+}) => {
+  const angle = angleRange * percentage;
+  const startAngle = angleOffset - 90;
+  const innerRadius = outerRadius - arcWidth;
+  const startAngleDegree = degTorad(startAngle);
+  const endAngleDegree = degTorad(startAngle + angle);
+  const largeArcFlag = angle < 180 ? 0 : 1;
+
+  const p1 = pointOnCircle(center, outerRadius, endAngleDegree);
+  const p2 = pointOnCircle(center, outerRadius, startAngleDegree);
+  const p3 = pointOnCircle(center, innerRadius, startAngleDegree);
+  const p4 = pointOnCircle(center, innerRadius, endAngleDegree);
+
+  return `M${p1.x},${p1.y} A${outerRadius},${outerRadius} 0 ${largeArcFlag} 0 ${
+    p2.x
+  },${p2.y}L${p3.x},${
+    p3.y
+  } A${innerRadius},${innerRadius} 0 ${largeArcFlag} 1 ${p4.x},${p4.y} L${
+    p1.x
+  },${p1.y}`;
+};
+
+export default ({ color, background, ...props }) => (
+  <g>
+    {background && (
+      <path
+        d={calcPath({ ...props, percentage: 1 })}
+        style={{ stroke: background }}
+      />
+    )}
+    <path d={calcPath(props)} style={{ fill: color }} />
+  </g>
+);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,95 @@
 // @format
 import React from "react";
+import PropTypes from "prop-types";
 
-export default class Knob extends React.Component {
+import Arc from "./Arc";
+
+class Knob extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleDown = this.handleDown.bind(this);
+    this.handleMove = this.handleMove.bind(this);
+    this.handleUp = this.handleUp.bind(this);
+    this.calcAngle = this.calcAngle.bind(this);
+
+    this.state = {
+      drag: false,
+      angle: props.defaultPercentage / props.angleRange
+    };
+  }
+
+  handleDown({ pageY }) {
+    this.setState({
+      drag: true
+    });
+  }
+
+  handleUp() {
+    this.setState({ drag: false, prevPageY: null });
+  }
+
+  handleMove({ pageY }) {
+    if (!this.state.drag) return;
+
+    const { angleRange } = this.props;
+    let { angle, prevPageY } = this.state;
+    if (!prevPageY) {
+      this.setState({ prevPageY: pageY });
+      return;
+    }
+    const delta = prevPageY - pageY;
+
+    angle = this.calcAngle(angle, delta, angleRange);
+    this.setState({ angle, prevPageY: pageY });
+  }
+
+  calcAngle(angle, delta, angleRange) {
+    angle += delta;
+    if (angle > angleRange) {
+      angle -= angle - angleRange;
+    } else if (angle < 0) {
+      angle += Math.abs(angle);
+    }
+    return angle;
+  }
+
   render() {
+    const { style, angleRange, angleOffset } = this.props;
+    const { angle } = this.state;
+
     return (
-      <svg viewBox="0 0 100, 100">
-        <circle r="40" cx="50" cy="50" />
+      <svg
+        onMouseDown={this.handleDown}
+        onMouseMove={this.handleMove}
+        onMouseUp={this.handleUp}
+        style={style}
+        viewBox="0 0 100, 100"
+      >
+        <circle fill="red" r="40" cx="50" cy="50" />
+        <Arc
+          percentage={angle / angleRange}
+          angleOffset={angleOffset}
+          angleRange={angleRange}
+          arcWidth={2}
+          radius={48}
+          center={50}
+          background="transparent"
+          color="red"
+        />
       </svg>
     );
   }
 }
+
+Knob.propTypes = {
+  defaultPercentage: PropTypes.number
+};
+
+Knob.defaultProps = {
+  defaultPercentage: 0,
+  angleOffset: 0,
+  angleRange: 360
+};
+
+export default Knob;
